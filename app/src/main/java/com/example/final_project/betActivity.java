44baf0e86dd2de7a446bb359a5545f83ab7f40ff
Yeon-Moo <zhang0ben0@gmail.com>
2022-12-24 02:26:30 +0800

    add horse ListView and SpinnerView

--- app/src/main/java/com/example/final_project/betActivity.java
+++ app/src/main/java/com/example/final_project/betActivity.java
@@ -7,8 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,6 +19,8 @@ public class betActivity extends AppCompatActivity {
     private ListView horseList;
     private Spinner horseChoose;
     private ArrayList<String> horse=new ArrayList<>();
+    private TextView tv_deposit;
+    private EditText ed_betPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +28,16 @@ public class betActivity extends AppCompatActivity {
         btn_bet=findViewById(R.id.btn_bet);
         horseList=findViewById(R.id.horseList);
         horseChoose=findViewById(R.id.horseChoose);
-        ArrayAdapter<String> horseAdapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        tv_deposit=findViewById(R.id.tv_deposit);
+        ed_betPrice=findViewById(R.id.ed_betPrice);
+        ArrayAdapter<String> horseAdapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,horse);
+        String[] horseSpinnerArray=new String[]{"一號馬","二號馬","三號馬","四號馬","五號馬","六號馬"};
+        ArrayAdapter<String> horseSpinnerAdapter=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,horseSpinnerArray);
+        horseChoose.setAdapter(horseSpinnerAdapter);
+        int newDeposit=50000;
+        tv_deposit.setText("目前金額:"+newDeposit+"元");
+        horseList.setAdapter(horseAdapter);
+
         boolean flag=false;
         horseInfo[] horses;
         horses=new horseInfo[6];
@@ -56,8 +69,12 @@ public class betActivity extends AppCompatActivity {
         for(int i=0;i<6;i++){
             double equityTmp=horses[i].Speed/SpeedSum;
            horses[i].equity=Math.round(equityTmp*10000.0)/100.0;
+           horse.add((i+1)+"號馬"+"\t 賠率:"+horses[i].betOdd+"\t 綜合能力值:"+horses[i].Speed+"\t 勝率:"+horses[i].equity+"%");
             System.out.println(horses[i].equity);
         }
+        horseAdapter.notifyDataSetChanged();
+
+
 
 
     }
